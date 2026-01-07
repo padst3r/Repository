d5220ae2b3a1554b40e65eee9699fc4850e27428
padst3r <58301144+padst3r@users.noreply.github.com>
2026-01-07 13:28:00 +0000

    Add files via upload

---
+++ AC-AzureVMSetLanguage.ps1
@@ -0,0 +1,40 @@
+# Justin Verstijnen AzureVM Set Language Script
+# Github page: https://github.com/JustinVerstijnen/JV-AzureVMSetLanguage
+# Let's start!
+Write-Host "Script made by..." -ForegroundColor DarkCyan
+Write-Host "     _           _   _        __     __            _   _  _                  
+    | |_   _ ___| |_(_)_ __   \ \   / /__ _ __ ___| |_(_)(_)_ __   ___ _ __  
+ _  | | | | / __| __| | '_ \   \ \ / / _ \ '__/ __| __| || | '_ \ / _ \ '_ \ 
+| |_| | |_| \__ \ |_| | | | |   \ V /  __/ |  \__ \ |_| || | | | |  __/ | | |
+ \___/ \__,_|___/\__|_|_| |_|    \_/ \___|_|  |___/\__|_|/ |_| |_|\___|_| |_|
+                                                       |__/                  " -ForegroundColor DarkCyan
+                                                       
+# === PARAMETERS ===
+$newlanguage = "en-GB"                       # Set Language here -> https://learn.microsoft.com/en-us/linkedin/shared/references/reference-tables/language-codes
+$TimeZoneToSet = "GMT Standard Time"   # Set Timezone here -> https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/default-time-zones?view=windows-11#time-zones
+$geoid = "242"                               # Set Geo ID here -> https://learn.microsoft.com/en-us/windows/win32/intl/table-of-geographical-locations
+# === END PARAMETERS ===
+
+Set-ExecutionPolicy Unrestricted -Scope Process
+
+# Step 4: Changing system language to newly installed language
+Set-SystemPreferredUILanguage $newlanguage -Verbose
+Set-WinUILanguageOverride -Language $newlanguage -Verbose
+Set-WinSystemLocale $newlanguage -Verbose
+Set-Culture $newlanguage
+Set-WinHomeLocation -GeoId $geoid
+
+
+# Step 5: Sets new Timezone
+Set-Timezone -Id $TimeZoneToSet
+
+
+# Step 6: Set new language for all current and new users and language bar settings
+Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True -Verbose
+Set-WinLanguageBarOption -UseLegacySwitchMode -UseLegacyLanguageBar
+
+
+# Step 7: Removing old language and rebooting VM to apply settings
+Uninstall-Language en-US -Verbose
+Start-Sleep -Seconds 30
+Restart-Computer -Force
